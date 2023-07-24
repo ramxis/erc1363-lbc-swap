b3f3349ce92ce13fa0fe6689a68e3fa030e0d4cd
Muhammad Rameez <rameez@Muhammads-MacBook-Pro.local>
2023-07-24 16:38:56 +0200

    fixed the deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,8 +5,12 @@ async function main() {
   const unlockTime = currentTimestampInSeconds + 60;
 
   const salt = ethers.encodeBytes32String("May the force be with you");
+  const initialTokenSupply = ethers.parseEther("10");
+  const reserveRatio = Math.round(1 / 2 * 1000000) / 1000000; //  recall 1/2 corresponds to LBC
+  const solReserveRatio = Math.floor(reserveRatio * 1000000);
+  let defaultGasPrice = ethers.parseUnits("30","gwei");
 
-  const LbcTokenSwap = await ethers.deployContract("v", [salt]);
+  const LbcTokenSwap = await ethers.deployContract("LbcTokenSwap", [salt,solReserveRatio,initialTokenSupply, defaultGasPrice]);
 
   await LbcTokenSwap.waitForDeployment();
 
